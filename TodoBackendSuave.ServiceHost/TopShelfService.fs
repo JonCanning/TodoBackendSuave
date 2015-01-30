19a18377c3e9d780ab6bfd96e17292f4ac089c26
Jon Canning <jon.canning@gmail.com>
2015-01-30 15:20:22 +0000

    Set name and display name

--- TodoBackendSuave.ServiceHost/TopShelfService.fs
+++ TodoBackendSuave.ServiceHost/TopShelfService.fs
@@ -2,7 +2,10 @@
 
 open System
 open System.Configuration
+open System.Reflection
 open Topshelf
+
+let name = Assembly.GetExecutingAssembly().GetName().Name
 
 type TopShelfService() = 
   let mutable disposable = Unchecked.defaultof<IDisposable>
@@ -22,6 +25,8 @@ type TopShelfService() =
 [<EntryPoint>]
 let main _ = 
   HostFactory.New(fun config -> 
+    config.SetDisplayName name
+    config.SetServiceName name
     config.Service<TopShelfService>() |> ignore)
   |> fun x -> x.Run() |> ignore
   0
